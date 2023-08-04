e218c2ed0dc8d58398fa09e689076bb184a8b7f3
Juan Mondaca <juan.sr1015@gmail.com>
2023-08-04 17:53:20 -0400

    spring initial template

---
+++ src/main/java/com/citi/test/citiapitest/CitiApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.citi.test.citiapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citi/test/citiapitest/CitiApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.test.citiapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
